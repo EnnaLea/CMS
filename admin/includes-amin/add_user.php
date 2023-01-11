abcd8e797ebe6df2d23beaad0e292cff8023d1f3
EnnaLea <49825624+leaaliman@users.noreply.github.com>
2023-01-11 07:54:24 +0100

    change 11/01/2023

--- admin/includes-amin/add_user.php
+++ admin/includes-amin/add_user.php
@@ -1,9 +1,9 @@
 <?php 
-if (isset($_POST['create_post'])) {
-    $post_title = $_POST['title'];
-    $post_author = $_POST['author'];
-    $post_category_id = $_POST['post_category'];
-    $post_status = $_POST['post_status'];
+if (isset($_POST['create_user'])) {
+    $user_id = $_POST['user_id'];
+    $username = $_POST['username'];
+    $user_password = $_POST['user_password'];
+    $post_status = $_POST['user_firstname'];
     $post_image = $_FILES['image']['name'];
     $post_image_temp = $_FILES['image']['tmp_name'];
     $post_tags = $_POST['post_tags'];
@@ -26,65 +26,45 @@ if (isset($_POST['create_post'])) {
 ?>
 
 
-
-<form action="add_user.php" method="post" enctype="multipart/form-data">
+<form action="" method="post" enctype="multipart/form-data">
 
 <div class="form-group">
-<label for="user_firstname">Firstname</label>
+<label for="post_title">Firstname</label>
 <input type="text" class="form-control" name="user_firstname">
 </div>
 
 <div class="form-group">
-<label for="user_lastname">Lastname</label>
+<label for="post_title">Lastname</label>
 <input type="text" class="form-control" name="user_lastname">
 </div>
 
 <div class="form-group">
-
 <select name="user_role" id="">
-
-<?php
-
-$query = "SELECT * FROM users ";
-$select_users = mysqli_query($connection, $query);
-
-confirm($select_users);
-
-while ($row = mysqli_fetch_assoc($select_users)) {
-    $user_id = $row['user_id'];
-    $user_role = $row['user_role'];
-
-    echo "<option value='user_id >{$user_role}</option>";
-
-
-}
-
-?>
-
-
+<option value="subscriber">Select Options</option>
+<option value="admin">Admin</option>
+<option value="subscriber">Subscriber</option>
 </select>
-
 </div>
 
 
 <!-- <div class="form-group">
-<label for="image">Post Image</label>
-<input type="file" class="form-control" name="image">
+<label for="post_image">Image</label>
+<input type="file"  name="image">
 </div> -->
 
 <div class="form-group">
-<label for="username">Username</label>
+<label for="post_status">Username</label>
 <input type="text" class="form-control" name="username">
 </div>
 
 <div class="form-group">
-<label for="user_email">Email</label>
+<label for="post_autor">Email</label>
 <input type="email" class="form-control" name="user_email">
 </div>
 
 <div class="form-group">
-<label for="user_password">Password</label>
-<input type="password" class="form-control" name="user_password"">
+<label for="image">Password</label>
+<input type="password" class="form-control" name="user_password">
 </div>
 
 <div class="form-group">
--- admin/includes-amin/edit_user.php
+++ admin/includes-amin/edit_user.php
@@ -73,6 +73,35 @@ if (isset($_POST['update_post'])) {
 </div>
 
 <div class="form-group">
+
+<select name="user_role" id="">
+
+<?php
+
+$query = "SELECT * FROM users ";
+$select_users = mysqli_query($connection, $query);
+
+confirm($select_users);
+
+while ($row = mysqli_fetch_assoc($select_users)) {
+    $user_id = $row['user_id'];
+    $user_role = $row['user_role'];
+
+    echo "<option value='$user_id'>{$user_role}</option>";
+
+
+}
+
+?>
+
+
+</select>
+
+</div>
+
+
+
+<div class="form-group">
 <label for="category_id">Category Id</label>
 
 <select name="post_category" id="post_category">
